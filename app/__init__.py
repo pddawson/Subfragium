5bc38529acf709934c4d744e68c65db3c3a96842
Paul Dawson <pddawson@gmail.com>
2017-02-07 18:27:19 +0000

    Disabled logging of SQL by SqlAlchemy.

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     dbPath = os.path.join(app.root_path, 'SubfragiumDB.sqlite3')
 
     app.config.update(dict(SQLALCHEMY_DATABASE_URI='sqlite:///' + dbPath))
+    app.config.update(dict(SQLALCHEMY_TRACK_MODIFICATIONS=False))
 
     errHandler = logging.FileHandler('SubfragiumController.log')
     errHandler.setFormatter(logging.Formatter('%(asctime)s,%(levelname)s,%(message)s'))
